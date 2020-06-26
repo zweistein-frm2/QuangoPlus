295e83bf33a90a4a19fb92e56dbe17de75a69781
alanghof <andreas.langhoff@frm2.tum.de>
2020-06-26 13:51:37 +0200

    .

--- cmh.py
+++ cmh.py
@@ -4,8 +4,7 @@ import sys
 module_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 if module_path not in sys.path:
     sys.path.append(module_path)
-import quango.main
 import cmh.quango_integration
 
-sys.exit(cmh.quango_integration.run())
+cmh.quango_integration.run()
 
